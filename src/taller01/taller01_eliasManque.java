2e9bd1539c60021ee7fd95a21e0b387f54a22783
Elias Manque <elias.manque@alumnos.ucn.cl>
2022-04-25 13:57:59 -0400

    Update taller01_eliasManque.java

--- src/taller01/taller01_eliasManque.java
+++ src/taller01/taller01_eliasManque.java
@@ -14,16 +14,22 @@ public class taller01_eliasManque {
     public static void menuUser(){
 
     }
+    /////////////////////////////////////////////// Registro
+    public static void registro(){
+
+    }
     /////////////////////////////////////////////// Busqueda Usuario
     public static void busquedaUser(String user, String pass) throws FileNotFoundException{
 
         File archCliente = new File("Clientes.txt");
         Scanner arch = new Scanner(archCliente);
+        boolean encontrado = false;
         while(arch.hasNext()){
             String[] partes = arch.next().split(",");
             String usuario = partes[0], contrase = partes[1];
             if (user.equals(usuario)){
                 System.out.println("Usuario encontrado");
+                encontrado = true;
                 if(pass.equals(contrase)){
                     System.out.println("Acceso Correcto");
                 }else{
@@ -31,7 +37,9 @@ public class taller01_eliasManque {
                 }
             }
         }
+        if (!encontrado){
 
+        }
     }
     /////////////////////////////////////////////// Lectura archivo Productos
     public static String[] productos(File arch) throws FileNotFoundException{
@@ -56,11 +64,15 @@ public class taller01_eliasManque {
         Scanner leer = new Scanner(System.in);
 
         while(confirmacion){
-            System.out.print("Ingrese us usuario ===> ");
+            System.out.print("Ingrese su usuario ===> ");
             String user = leer.next();
             System.out.print("Ingrese su contraseña ===> ");
             String pass = leer.next();
-            busquedaUser(user,pass);
+            if (user.equals("ADMIN") && pass.equals("NYAXIO")){
+                menuAdmin();
+            }else{
+                busquedaUser(user,pass);
+            }
             System.out.print("Desea volver a inciar? SI/NO ==>  ");
             String opcion = leer.next();
             opcion = opcion.toUpperCase();
