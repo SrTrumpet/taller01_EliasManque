734356e93805e1a2c516309402a0f20c7de5adf0
Elias Manque <elias.manque@alumnos.ucn.cl>
2022-04-25 13:18:19 -0400

    Update taller01_eliasManque.java

--- src/taller01/taller01_eliasManque.java
+++ src/taller01/taller01_eliasManque.java
@@ -1,11 +1,20 @@
-package taller01;
+package Taller01;
 
-/**
- *
- * @author Elias M. Olivares
- */
+import java.io.File;
+import java.util.Scanner;
+
 public class taller01_eliasManque {
-    public static void main(String args[]) {
-
+    public static void main(String[] args) throws Exception {
+        /////////////////////////////////////////////// Lectura archivo Clientes
+        Scanner leer = new Scanner(System.in);
+        File archCliente = new File("Clientes.txt");
+        Scanner arch = new Scanner(archCliente);
+        while (arch.hasNext()){
+            String [] partes = arch.next().split(",");
+            String User = partes[0], Pass = partes[1];
+            int saldo = Integer.parseInt(partes[2]);
+            String Email = partes[3];
+        }
     }
+    
 }
