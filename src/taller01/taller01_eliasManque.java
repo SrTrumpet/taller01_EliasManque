917a1b0cb6ae73427e44fc0610e19c875e4be1e3
Elias Manque <elias.manque@alumnos.ucn.cl>
2022-04-25 13:36:08 -0400

    Update taller01_eliasManque.java

--- src/taller01/taller01_eliasManque.java
+++ src/taller01/taller01_eliasManque.java
@@ -1,20 +1,45 @@
 package Taller01;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class taller01_eliasManque {
+
+    /////////////////////////////////////////////// Lectura archivo Productos
+    public static String[] productos(File arch) throws FileNotFoundException{
+        String[] stock;
+        Scanner leerArch = new Scanner(arch);
+        int veces = 0;
+        while(leerArch.hasNext()){
+            String[] partes = leerArch.next().split(",");
+            String producto = partes[0];
+            System.out.println(producto);
+            veces++;
+        }
+        System.out.println(veces);
+        return null;
+        
+    }
+
     public static void main(String[] args) throws Exception {
         /////////////////////////////////////////////// Lectura archivo Clientes
         Scanner leer = new Scanner(System.in);
         File archCliente = new File("Clientes.txt");
+        File archProducto = new File("Productos.txt");
         Scanner arch = new Scanner(archCliente);
+        int contador = 0;
+        String[] productosStock;
+
         while (arch.hasNext()){
             String [] partes = arch.next().split(",");
             String User = partes[0], Pass = partes[1];
             int saldo = Integer.parseInt(partes[2]);
             String Email = partes[3];
+
+            contador++;
         }
+        productos(archProducto);
     }
     
 }
