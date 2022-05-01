194ec6fbb24868044c519de728ddb1f37ee53455
Elias Manque <elias.manque@alumnos.ucn.cl>
2022-05-01 15:45:27 -0400

    Update taller01_eliasManque.java

--- src/taller01/taller01_eliasManque.java
+++ src/taller01/taller01_eliasManque.java
@@ -1,86 +1,62 @@
-package Taller01;
+package taller01;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class taller01_eliasManque {
+    
 
-    /////////////////////////////////////////////// Menu Admin
-    public static void menuAdmin(){
+    ////////////////////////////////////////////////////////////////Lectura de archivo
 
-    }
-    /////////////////////////////////////////////// Menu Usuario
-    public static void menuUser(){
-
-    }
-    /////////////////////////////////////////////// Registro
-    public static void registro(){
-
-    }
-    /////////////////////////////////////////////// Busqueda Usuario
-    public static void busquedaUser(String user, String pass) throws FileNotFoundException{
-
-        File archCliente = new File("Clientes.txt");
-        Scanner arch = new Scanner(archCliente);
-        boolean encontrado = false;
-        while(arch.hasNext()){
-            String[] partes = arch.next().split(",");
-            String usuario = partes[0], contrase = partes[1];
-            if (user.equals(usuario)){
-                System.out.println("Usuario encontrado");
-                encontrado = true;
-                if(pass.equals(contrase)){
-                    System.out.println("Acceso Correcto");
-                }else{
-                    System.out.println("Contraseña Incorrecta");
-                }
-            }
-        }
-        if (!encontrado){
-
-        }
-    }
-    /////////////////////////////////////////////// Lectura archivo Productos
-    public static String[] productos(File arch) throws FileNotFoundException{
-        String[] stock;
-        Scanner leerArch = new Scanner(arch);
-        int veces = 0;
-        while(leerArch.hasNext()){
-            String[] partes = leerArch.next().split(",");
-            String producto = partes[0];
-            System.out.println(producto);
-            veces++;
-        }
-        System.out.println(veces);
+    public static int[] pasoTXTaLista (File archCliente) throws Exception{
         return null;
         
     }
+    ////////////////////////////////////////////////////////////////Conteo de Lineas del Arch
+
+    public static int conteoLineas(File archivoConteo){
+        return 0;
+    }
+    ////////////////////////////////////////////////////////////////Mostrar Catalogo de Stock
+
+    public static void mostrarCAtalogo(File archProductos) throws Exception{
+        Scanner arch = new Scanner(archProductos);
+        System.out.println("############################");
+        System.out.println(" ");
+        System.out.println("Lista de Productos en Stock: ");
+        System.out.println(" ");
+        while (arch.hasNextLine()){
+            String linea = arch.nextLine();
+            String partes[] = linea.split(",");
+            //Definiendo partes para mostrarlas por pantalla
+            String  producto    = partes[0],
+                    precio      = partes[1],
+                    unidadesDis = partes[2];
+            //Salida por Pantalla
+            System.out.print("""
+                    ############################
+                    Producto : %s
+                    Precio   : $%s USD
+                    Unidades : %s
+                    """.formatted(producto,precio,unidadesDis));
+        }
+    }
+
+
+
+
+
 
     public static void main(String[] args) throws Exception {
-        /////////////////////////////////////////////// Lectura archivo Clientes
+        
+        //Inicializacion de Variables
+        //Arch Productos
+        File productos = new File("Productos.txt");
+        mostrarCAtalogo(productos);
 
-        boolean confirmacion = true;
-        Scanner leer = new Scanner(System.in);
 
-        while(confirmacion){
-            System.out.print("Ingrese su usuario ===> ");
-            String user = leer.next();
-            System.out.print("Ingrese su contraseña ===> ");
-            String pass = leer.next();
-            if (user.equals("ADMIN") && pass.equals("NYAXIO")){
-                menuAdmin();
-            }else{
-                busquedaUser(user,pass);
-            }
-            System.out.print("Desea volver a inciar? SI/NO ==>  ");
-            String opcion = leer.next();
-            opcion = opcion.toUpperCase();
-            if (opcion.equals("SI")){
-                confirmacion = true;
-            }else{
-                confirmacion = false;
-            }
-        }
+
+
+
     }
 }
